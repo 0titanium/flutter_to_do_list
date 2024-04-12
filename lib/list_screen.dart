a077e681fa19a0f78e2294ba2ce15b320e5a944b
kimhyungmin <jeen14rla@gmail.com>
2024-04-12 16:47:40 +0900

    feat: add list screen

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -5,8 +5,31 @@ class ListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To do list'),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('title 1'),
+            subtitle: Text('sub title 1'),
+          ),
+          ListTile(
+            title: Text('title 2'),
+            subtitle: Text('sub title 2'),
+          ),
+          ListTile(
+            title: Text('title 3'),
+            subtitle: Text('sub title 3'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
